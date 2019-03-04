eb20613723f41d35a69ad029dd2751859ad984e3
yuichi-kitaoka <forbidden03@gmail.com>
2019-03-04 14:07:53 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # 新しく作成
+from django.utils import timezone
+from .models import Post
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # 公開したブログ記事を published_dateで並べ替えをしたい
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
